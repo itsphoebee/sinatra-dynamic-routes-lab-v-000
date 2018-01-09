2f814748a181e3dd4c004a191696051416c1fc90
Phoebe Lau <phoebelau7@gmail.com>
2018-01-09 20:13:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,9 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @p = params[:phrase]
     @n = params[:number].to_i
-    @n.times do
-      "#{@p}"
-    end
+    @p * @n
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
